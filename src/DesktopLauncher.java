9181ff929e34aa7c7042a69eef63c780ad1dc3c0
Draccoon <daniel.roods.dev@gmail.com>
2026-09-01 14:28:43 +0330

    fix after match pinata scale and fullscreen

--- src/DesktopLauncher.java
+++ src/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("Plants vs Zombies - Straw Hats");
 
-        config.setWindowedMode(1152, 648);
+        config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
         config.setResizable(true);
         config.setDecorated(true);
 
